70dfb7099e453472359242d299b74ee6f43ed78a
genose.org <genose@genose.org>
2019-06-04 16:35:41 +0200

    daily commit

--- JAVA/prj_jee/ProjectJavaFX_102/src/Main/Main.java
+++ JAVA/prj_jee/ProjectJavaFX_102/src/Main/Main.java
@@ -16,23 +16,25 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class Main extends ApplicationJFX { 
+public class Main extends Application { 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
     	
     	try {
-    		System.out.println(" :: "+getApplicationPath());
-    		
+    		ApplicationJFX app = new ApplicationJFX();
     		URL aUrlClassPath = getClass().getResource(".");
+    		System.out.println(getClass()+" ;; "+aUrlClassPath);
+    		System.out.println(getClass()+" :: "+ApplicationJFX.getApplicationPath());
     		
-        	super.start(primaryStage);
+    		
+        	// super.start(primaryStage);
         	 
 
         	
             // Parent root = FXMLLoader.load(getClass().getResource("../MainWindow/MainWindow.fxml"));
             
-            primaryStage.initStyle(StageStyle.UNDECORATED);
+          /*  primaryStage.initStyle(StageStyle.UNDECORATED);
             primaryStage.setOpacity(0.8);
             // primaryStage.setScene(new Scene(root, primaryStage.getHeight(), primaryStage.getWidth()));
         	
@@ -40,7 +42,7 @@ public class Main extends ApplicationJFX {
         	            
             primaryStage.show();
             primaryStage.setIconified(false);
-            primaryStage.centerOnScreen();
+            primaryStage.centerOnScreen();*/
         	
 	
 		} catch (Exception EV_ERR_INSTANTIATE) {
--- JAVA/prj_jee/ProjectJavaFX_102/src/org/genose/java/implementation/javafx/applicationtools/ApplicationJFX.java
+++ JAVA/prj_jee/ProjectJavaFX_102/src/org/genose/java/implementation/javafx/applicationtools/ApplicationJFX.java
@@ -2,6 +2,10 @@ package org.genose.java.implementation.javafx.applicationtools;
 
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
  
 import javafx.application.Application;
@@ -10,7 +14,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ApplicationJFX extends Application {
-
+	private static Application instance;
 	
     public enum APPLICATION_MVC_DIRS  {
     		DIR_ASSETS("Assets"), DIR_VIEWS("Views"), DIR_CONTROLLERS("Controllers"), DIR_RESSOURCES("Ressources"), DIR_APPSRC("src");
@@ -57,25 +61,56 @@ public class ApplicationJFX extends Application {
     };
 	private static Stage aPrimaryStage; // **Declare static Stage**
 
+	/**
+	 * 
+	 * 
+	 */
+	public ApplicationJFX() {
+		super();
+	    synchronized(ApplicationJFX.class){
+	        if(instance != null) throw new UnsupportedOperationException(
+	                getClass()+" is singleton but constructor called more than once");
+	        instance = this;
+	    }
+	    System.out.println(getClass()+" : was instiated");
+	}
+	/* ****************************************************** */
+	/**
+	 * 
+	 * @param stage
+	 */
     private void setPrimaryStage(Stage stage) {
         ApplicationJFX.aPrimaryStage = stage;
     }
 
+	/* ****************************************************** */
+    /**
+     * 
+     * @return javafx.Stage
+     */
     static public Stage getPrimaryStage() {
         return ApplicationJFX.aPrimaryStage;
     }
 	
-	
-	public ApplicationJFX() {
-		// TODO Auto-generated constructor stub
-	}
-
+	/* ****************************************************** */
+    /**
+     * 
+     * 
+     */
 	@Override
 	public void start(Stage arg0) throws Exception {
+		synchronized(ApplicationJFX.class){
+	        if(instance == null) instance = this;
+		}
 		setPrimaryStage(arg0);
 	}
 	
-	
+	/* ****************************************************** */
+	/**
+	 * 
+	 * @param aIconPath
+	 * @return true when icon is set
+	 */
 	public static boolean setIcon(String aIconPath) {
 		
 		// :: https://stackoverflow.com/questions/10121991/javafx-application-icon
@@ -91,22 +126,77 @@ public class ApplicationJFX extends Application {
 		
 		return false;
 	}
-
+	/* ****************************************************** */
+	/**
+	 * 
+	 * @return
+	 */
 	private static Stage getActiveStage() {
 		// TODO Auto-generated method stub
 		return getPrimaryStage();
 	}
 
+	/* ****************************************************** */
+	/**
+	 * 
+	 * @return bundle application path
+	 */
 	public static String getApplicationPath() {
-		// TODO Auto-generated method stub
-		File localClassPath = new java.io.File(".");
-		String documentPath = localClassPath.getAbsolutePath();
+				
+		Class localClass = ApplicationJFX.getApplicationJFXSingleton().getClass();
 		
-		// ApplicationJFX.getClass().getProtectionDomain().getCodeSource().getLocation();
+		String packageNamed = localClass.getPackageName();
+		String localPackagePath = ""+packageNamed.replaceAll("[.]", "/");
+ 		File localClassPath = new java.io.File( localPackagePath );
+		String localAbsolutePath = localClassPath.getAbsolutePath();
+		String localpath = localClassPath.getPath()+"n";
+		String localpathname = localClassPath.getName();
 		
-		return documentPath;
+		System.out.println(localClass+" 1 ;; class package path  \n >> "+localPackagePath);
+		System.out.println(localClass+" 1 ;; class abs \n >> "+localAbsolutePath);
+		System.out.println(localClass+" 1 ;; class localpath \n >> "+localpath);
+		System.out.println(localClass+" 1 ;; class pathname \n >> "+localpathname);
+
+		
+		System.out.println("");
+		System.out.println("");
+		URL aUrlClass = localClass.getResource(localAbsolutePath);
+		System.out.println(localClass+" 3 ;; class getressource abspath doc \n >> "+aUrlClass);
+		
+		System.out.println("");
+		URL aUrlClassPath = localClass.getResource(localpath);
+		System.out.println(localClass+" 2 ;; class getressource local \n >> "+aUrlClassPath+" \n >> "+localpath );
+		
+		System.out.println("");
+		String testuri = null;
+		
+		URL testurl = null;
+		try {
+			testuri = aUrlClassPath.getPath();
+			System.out.println(" ;;;; "+testuri);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		aUrlClass = localClass.getResource(testuri);
+		System.out.println(localClass+" 3 ;; class getressource abspath doc \n"+aUrlClass);
+		
+		// URL aUrlClass = ApplicationJFX.class.getClass().getProtectionDomain().getCodeSource().getLocation();
+		// System.out.println(" ;; "+aUrlClass);
+		return localpath;
 	}
 
+	private static Object getApplicationJFXSingleton() {
+		// TODO Auto-generated method stub
+		return instance;
+	}
+	/* ****************************************************** */
+	/**
+	 * 
+	 * @param aPath
+	 * @return true if path exists inside bundle app 
+	 */
 	public static boolean applicationPathExist(String aPath) {
 		// TODO Auto-generated method stub
 		try {

f6b33a21f996a72af0f214b43f6b53a5e926a8ac
genose.org <genose@genose.org>
2019-09-03 16:30:48 +0200

    daily commit

--- JAVA/prj_jee/ProjectJavaFX105/src/com/application/sample/Main.java
+++ JAVA/prj_jee/ProjectJavaFX105/src/com/application/sample/Main.java
@@ -12,7 +12,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        URL fileDesign = getClass().getResource(getClass().getResource(".")+ "/design/sample.fxml");
+        String pathFXML = getClass().getResource("/")+String.valueOf(getClass().getPackageName()).replaceAll("[\\.]", "/");
+        pathFXML = "design/sample.fxml";
+        System.out.println( "path ::  "+getClass().getResource("."));
+        System.out.println( "path :: !! "+pathFXML);
+        URL fileDesign = getClass().getResource(pathFXML);
+        System.out.println( "getressource :: !! "+fileDesign);
         Parent root = FXMLLoader.load(fileDesign);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));

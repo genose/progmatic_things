38ad54180cd171a10f5685df7f4549f48b4bc96d
genose.org <genose@genose.org>
2024-05-24 18:06:59 +0200

    experimentation  Angular 102

--- TYPESCRIPT/Angular_Projects/Angular_102/src/app/app.component.ts
+++ TYPESCRIPT/Angular_Projects/Angular_102/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BlockComponent} from './block/block.component';
-
+import { BlockComponent } from './block/block.component';
+import {CommonModule} from '@angular/common'; // Import NgFor
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BlockComponent],
+  imports: [RouterOutlet, BlockComponent, CommonModule ], // Add NgFor to imports
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

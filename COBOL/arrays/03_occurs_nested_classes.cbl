a117b2b5e80566f931d5604575955fdd64751852
genose.org <genose@genose.org>
2026-09-03 19:42:40 +0200

    fix(COBOL): fix compile errors in arrays/03,04 + rename 02_rapport_ventes (filename too long for GnuCOBOL)

--- COBOL/arrays/03_occurs_nested_classes.cbl
+++ COBOL/arrays/03_occurs_nested_classes.cbl
@@ -2,7 +2,7 @@
       * PROGRAMME  : CLASSES-NOTES                                     *
       * AUTEUR     : Sebastien Cotillard                               *
       * DATE       : 2026-09-02                                        *
-      * VERSION    : 1.0                                               *
+      * VERSION    : 1.1                                               *
       *----------------------------------------------------------------*
       * DESCRIPTION: OCCURS imbriques 3 niveaux : classes, eleves, notes
       *----------------------------------------------------------------*
@@ -15,56 +15,58 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01 SC-ECOLE.
-           05 SC-CLASSE OCCURS 30 TIMES INDEXED BY WS-INDEX-CLASSE.
+           05 SC-CLASSE OCCURS 3 TIMES INDEXED BY WS-INDEX-CLASSE.
                10 SC-NOM-CLASSE PIC X(20) VALUE "CLASSE ".
                10 SC-ANNEE PIC 9(4) VALUE ZEROES.
-               10 CL-ELEVES OCCURS 25 TIMES INDEXED BY WS-INDEX-ELEVE.
+               10 CL-ELEVES OCCURS 5 TIMES INDEXED BY WS-INDEX-ELEVE.
                    15 CL-NOM PIC X(20) VALUE "NOM ".
-                        88 CL-FIN-ClASSE VALUE SPACE.
                    15 CL-PRENOM PIC X(20) VALUE "PRENOM ".
                    15 CL-AGE PIC 9(2) VALUE ZEROES.
                    15 CL-MOYENNE PIC 9(3)V99 VALUE ZEROES.
-                   15 CL-NOTES OCCURS 5 TIMES INDEXED BY WS-INDEX-NOTE.
+                   15 CL-NOTES OCCURS 3 TIMES
+                      INDEXED BY WS-INDEX-NOTE.
                       20 CL-MATIERE PIC X(20) VALUE "MATIERE ".
-                      20 CL-COEFFICIENT PIC 9(2) VALUE ZEROES.
                       20 CL-SCORE PIC 9(3)V99 VALUE ZEROES.
-                           88 ABSENCE-NOTE VALUE 0.
-                           88 NOTE-VALIDE VALUE 1 THRU 20.
-                           88 NOTE-EXCEPTIONNELLE VALUE 21 THRU 30.
 
-       01 WS-NOTE-VALUE PIC 9(2) VALUE ZEROES.
-       01 WS-NOTE-SOMME PIC 9(3) VALUE ZEROES.
-       01 WS-NOTE-MOYENNE PIC 9(3)V99 VALUE ZEROES.
+       01 WS-NOTE-SOMME PIC 9(5)V99 VALUE ZEROES.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           SET WS-INDEX-CLASSE TO 1
-           SET WS-INDEX-ELEVE TO 0
-           SET WS-INDEX-NOTE TO 0
+           PERFORM VARYING WS-INDEX-CLASSE FROM 1 BY 1
+               UNTIL WS-INDEX-CLASSE > 3
 
-           PERFORM VARYING WS-INDEX FROM 1 BY 1
-            UNTIL WS-INDEX > LEGNTH OF SC-CLASSE
+               MOVE "CLASSE " TO
+                   SC-NOM-CLASSE(WS-INDEX-CLASSE)
+               MOVE 2024 TO SC-ANNEE(WS-INDEX-CLASSE)
 
-                 MOVE "CLASSE " TO SC-NOM-CLASSE(WS-INDEX)
-                 MOVE 2024 TO SC-ANNEE(WS-INDEX)
-                 PERFORM VARYING WS-NOTE-INDEX FROM 1 BY 1 UNTIL WS-NOTE-INDEX > 25
-                     MOVE "NOM " TO CL-NOM(WS-NOTE-INDEX)
-                     MOVE "PRENOM " TO CL-PRENOM(WS-NOTE-INDEX)
-                     MOVE 10 TO CL-AGE(WS-NOTE-INDEX)
-                     PERFORM VARYING WS-NOTE-VALUE FROM 1 BY 1
-                     UNTIL WS-NOTE-VALUE > LENGTH OF CL-NOTES
-                         MOVE WS-NOTE-VALUE TO
-                           CL-NOTE(WS-NOTE-VALUE)(WS-NOTE-INDEX)
-                         ADD CL-NOTE(WS-NOTE-VALUE)(WS-NOTE-INDEX)
+               PERFORM VARYING WS-INDEX-ELEVE FROM 1 BY 1
+                   UNTIL WS-INDEX-ELEVE > 5
+
+                   MOVE "NOM " TO
+                       CL-NOM(WS-INDEX-CLASSE, WS-INDEX-ELEVE)
+                   MOVE "PRENOM " TO
+                       CL-PRENOM(WS-INDEX-CLASSE, WS-INDEX-ELEVE)
+                   MOVE 18 TO
+                       CL-AGE(WS-INDEX-CLASSE, WS-INDEX-ELEVE)
+
+                   MOVE ZEROES TO WS-NOTE-SOMME
+                   PERFORM VARYING WS-INDEX-NOTE FROM 1 BY 1
+                       UNTIL WS-INDEX-NOTE > 3
+                       MOVE 10 TO CL-SCORE(WS-INDEX-CLASSE,
+                           WS-INDEX-ELEVE, WS-INDEX-NOTE)
+                       ADD CL-SCORE(WS-INDEX-CLASSE,
+                           WS-INDEX-ELEVE, WS-INDEX-NOTE)
                            TO WS-NOTE-SOMME
-                     END-PERFORM
-                     COMPUTE CL-MOYENNE(WS-NOTE-INDEX)
-                           = WS-NOTE-SOMME / LENGTH OF CL-NOTES
-                     MOVE ZEROES TO WS-NOTE-MOYENNE
-                     MOVE ZEROES TO WS-NOTE-SOMME
+                   END-PERFORM
 
+                   COMPUTE CL-MOYENNE(WS-INDEX-CLASSE,
+                       WS-INDEX-ELEVE) = WS-NOTE-SOMME / 3
 
-                 END-PERFORM
+               END-PERFORM
            END-PERFORM
-            STOP RUN.
+
+           DISPLAY "CLASSE 1 ELEVE 1 MOYENNE: "
+               CL-MOYENNE(1, 1)
+
+           STOP RUN.
        END PROGRAM CLASSES-NOTES.
